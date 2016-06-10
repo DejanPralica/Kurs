7883ba87017e596f49cf5beb6d1af771ecd56040
DejanPralica <dey0013@gmail.com>
2016-06-10 11:00:37 +0200

    Update pretraga.php

--- pretraga.php
+++ pretraga.php
@@ -1,30 +1,48 @@
 <html>
 	<head>
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link href="css/bootstrap.min.css" rel="stylesheet">
+
 	<style>
-	.button{
-		border:0;
-		color:#333;
-		border-radius:3px;
-		background-color:#88c425;	}
-div {
-   
-   width:200px; 
-   height:200px; 
-   position: fixed;
-   top: 50%; 
-   left: 50%; 
-   margin-top: -100px; 
-   margin-left: -100px; 
+div{
+
 }
+.stylish-input-group .input-group-addon{
+    background: white !important; 
+}
+.stylish-input-group .form-control{
+	border-right:0; 
+	box-shadow:0 0 0; 
+	border-color:#ccc;
+	
+}
+.stylish-input-group button{
+    border:0;
+    background:transparent;
+}}
 </style>
+
 	</head>
 	
 	<body>
-<div>
-		<form action='rezultat.php' method='get'>
-			<input type='text' name="name" size='30'>
-			<input type='submit'  class="button" value='Posalji'>
-		</form>
+	<form action='rezultat.php' method='get'>
+<div class="container">
+	<div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <div id="imaginary_container"> 
+                <div class="input-group stylish-input-group">
+                    <input type="text" name="x" class="form-control"  placeholder="Pretrazi" >
+                    <span class="input-group-addon"> 
+                        <button type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>  
+						</form>
+                    </span>
+                </div>
+            </div>
+        </div>
 	</div>
+</div>
+
 	</body>
 </html>
